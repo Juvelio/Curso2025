acad9318d2497f2dfb462ddecb550c8c9604e7d1
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2025-06-13 11:06:35 -0500

    Corriguiendo error

--- MiApp/Views/ControlesPage.xaml.cs
+++ MiApp/Views/ControlesPage.xaml.cs
@@ -2,8 +2,13 @@ namespace MiApp.Views;
 
 public partial class ControlesPage : ContentPage
 {
-	public ControlesPage()
-	{
-		InitializeComponent();
-	}
+    public ControlesPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void btnRepositorio_Clicked(object sender, EventArgs e)
+    {
+        await Browser.OpenAsync("https://github.com/Juvelio/Curso2025");
+    }
 }
