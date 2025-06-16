47910ef6c6c1ad220a06f906ef35c23f9fed95ec
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2025-06-16 12:14:43 -0500

    Final de la pagina de inicio

--- MiApp/MauiProgram.cs
+++ MiApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace MiApp
 {
@@ -9,6 +10,7 @@ namespace MiApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
