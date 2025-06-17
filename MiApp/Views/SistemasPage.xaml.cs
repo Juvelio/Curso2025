ac1483dda4bf7edd7d8d902ae53f699395a6ca4f
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2025-06-17 12:23:12 -0500

    Aplicacion listo para publicar.

--- MiApp/Views/SistemasPage.xaml.cs
+++ MiApp/Views/SistemasPage.xaml.cs
@@ -20,11 +20,26 @@ public partial class SistemasPage : ContentPage
                 await Browser.OpenAsync("https://denuncias.policia.gob.pe/sirdic/Login.aspx");
                 break;
             case "SIEPOL":
+                await Browser.OpenAsync("https://siepol.policia.gob.pe/Login.aspx");
+                break;
+            case "SERPOL":
+                await Browser.OpenAsync("https://serpol.policia.gob.pe/serpol/login");
+                break;
+            case "CORREO":
+                await Browser.OpenAsync("https://correo.policia.gob.pe/");
+                break;
+            case "SIGCP":
+                await Browser.OpenAsync("https://sigcp.policia.gob.pe/");
+                break;
+            case "MPD PNP":
+                await Browser.OpenAsync("https://mpd.policia.gob.pe/login");
+                break;
+            case "LICENCIAS":
                 await Browser.OpenAsync("https://denuncias.policia.gob.pe/xyz/Login.aspx");
                 break;
             default:
                 await DisplayAlert("Error", "Sistema no reconocido", "OK");
                 break;
-        }   
-    }    
+        }
+    }
 }
