704a864d24ada633567c752b15ee7752f78c9610
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2025-06-17 11:40:09 -0500

    Ok

--- MiApp/Views/SistemasPage.xaml.cs
+++ MiApp/Views/SistemasPage.xaml.cs
@@ -2,8 +2,29 @@ namespace MiApp.Views;
 
 public partial class SistemasPage : ContentPage
 {
-	public SistemasPage()
-	{
-		InitializeComponent();
-	}
+    public SistemasPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Sistema_Clicked(object sender, EventArgs e)
+    {
+        var sistema = (sender as Button)?.Text;
+
+        switch (sistema)
+        {
+            case "SIDPOL":
+                await Browser.OpenAsync("https://denuncias.policia.gob.pe/sidpol/Login.aspx");
+                break;
+            case "SIRDIC":
+                await Browser.OpenAsync("https://denuncias.policia.gob.pe/sirdic/Login.aspx");
+                break;
+            case "SIEPOL":
+                await Browser.OpenAsync("https://denuncias.policia.gob.pe/xyz/Login.aspx");
+                break;
+            default:
+                await DisplayAlert("Error", "Sistema no reconocido", "OK");
+                break;
+        }   
+    }    
 }
