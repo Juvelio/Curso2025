38c19d6cb448dc4e42fa5b39e2585691efdc5720
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2025-06-17 09:27:01 -0500

    OK

--- MiApp/App.xaml.cs
+++ MiApp/App.xaml.cs
@@ -5,11 +5,27 @@
         public App()
         {
             InitializeComponent();
+            QuitarSubrayado();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        void QuitarSubrayado()
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("QuitarSubrayado", (handler, view) =>
+            {
+#if ANDROID
+                //SOLO EJECUTA ANDROID
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                handler.PlatformView.Background = null;
+#elif IOS
+                //SOLO EJECUTA iOS
+#endif
+
+            });
         }
     }
 }
--- MiApp/Views/CalcularIMCPage.xaml.cs
+++ MiApp/Views/CalcularIMCPage.xaml.cs
@@ -6,4 +6,22 @@ public partial class CalcularIMCPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnCalcular_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+	private string ObtenerCategoriaIMC(double imc)
+	{
+        //Criterios de la Organizacion Mundial de la Salud (OMS)
+		if (imc < 18.5)
+			return "Bajo peso";
+		else if (imc >= 18.5 && imc < 24.9)
+			return "Peso normal";
+		else if (imc >= 25 && imc < 29.9)
+			return "Sobrepeso";
+		else
+			return "Obesidad";
+    }
 }
